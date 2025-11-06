509fd0f102b9b35cd673f81decd57f66e55db358
Slogiker <plibersek.daniel@gmail.com>
2025-11-06 10:04:03 +0100

    doubley linked list

--- blockchain c++/crypto.cpp
+++ blockchain c++/crypto.cpp
@@ -3,20 +3,26 @@
 
 using namespace std;
 
-
-class User {
+// Node for User doubly linked list
+class UserNode {
 public:
     int id;
     int balance;
+    UserNode* next;
+    UserNode* prev;
 
-    User() {
+    UserNode() {
         id = -1;
         balance = 0;
+        next = nullptr;
+        prev = nullptr;
     }
 
-    User(int id, int initialBalance = 0) {
+    UserNode(int id, int initialBalance = 0) {
         this->id = id;
         this->balance = initialBalance;
+        this->next = nullptr;
+        this->prev = nullptr;
     }
 
     void print() const {
@@ -24,6 +30,60 @@ public:
     }
 };
 
+// Doubly linked list for Users
+class UserList {
+public:
+    UserNode* head;
+    UserNode* tail;
+    int size;
+
+    UserList() {
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
+
+    ~UserList() {
+        UserNode* current = head;
+        while (current != nullptr) {
+            UserNode* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+
+    void addUser(int id, int initialBalance = 0) {
+        UserNode* newUser = new UserNode(id, initialBalance);
+
+        if (head == nullptr) {
+            head = tail = newUser;
+        } else {
+            tail->next = newUser;
+            newUser->prev = tail;
+            tail = newUser;
+        }
+        size++;
+    }
+
+    UserNode* findUser(int id) {
+        UserNode* current = head;
+        while (current != nullptr) {
+            if (current->id == id) {
+                return current;
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
+
+    void printAll() const {
+        UserNode* current = head;
+        while (current != nullptr) {
+            current->print();
+            current = current->next;
+        }
+    }
+};
 
 class Transaction {
 public:
@@ -46,15 +106,20 @@ public:
     }
 };
 
-class Block {
+// Node for Block doubly linked list
+class BlockNode {
 public:
     int index;
     time_t timestamp;
     Transaction transaction;
+    BlockNode* next;
+    BlockNode* prev;
 
-    Block() {
+    BlockNode() {
         index = 0;
         timestamp = time(0);
+        next = nullptr;
+        prev = nullptr;
     }
 
     void print() const {
@@ -65,34 +130,49 @@ public:
     }
 };
 
-
+// Doubly linked list for Blockchain
 class Blockchain {
 public:
-    static const int MAX_BLOCKS = 10;
-    Block blocks[MAX_BLOCKS];
+    BlockNode* head;
+    BlockNode* tail;
     int size;
 
     Blockchain() {
+        head = nullptr;
+        tail = nullptr;
         size = 0;
     }
 
-    void addTransaction(int from, int to, int amount) {
-        if (size >= MAX_BLOCKS) {
-            cout << "Blockchain is full!" << endl;
-            return;
+    ~Blockchain() {
+        BlockNode* current = head;
+        while (current != nullptr) {
+            BlockNode* temp = current;
+            current = current->next;
+            delete temp;
         }
+    }
 
-        Transaction t(from, to, amount);
-        Block b;
-        b.index = size;
-        b.transaction = t;
-        b.timestamp = time(0);
-        blocks[size++] = b;
+    void addTransaction(int from, int to, int amount) {
+        BlockNode* newBlock = new BlockNode();
+        newBlock->index = size;
+        newBlock->transaction = Transaction(from, to, amount);
+        newBlock->timestamp = time(0);
+
+        if (head == nullptr) {
+            head = tail = newBlock;
+        } else {
+            tail->next = newBlock;
+            newBlock->prev = tail;
+            tail = newBlock;
+        }
+        size++;
     }
 
     void printChain() const {
-        for (int i = 0; i < size; ++i) {
-            blocks[i].print();
+        BlockNode* current = head;
+        while (current != nullptr) {
+            current->print();
+            current = current->next;
         }
     }
 
@@ -101,17 +181,29 @@ public:
             cout << "Invalid block number!" << endl;
             return;
         }
-        blocks[blockNumber].print();
+
+        BlockNode* current = head;
+        int count = 0;
+        while (current != nullptr && count < blockNumber) {
+            current = current->next;
+            count++;
+        }
+
+        if (current != nullptr) {
+            current->print();
+        }
     }
 
     void displayBalance(int userId) const {
         int balance = 0;
 
-        for (int i = 0; i < size; ++i) {
-            if (blocks[i].transaction.to == userId)
-                balance += blocks[i].transaction.amount;
-            if (blocks[i].transaction.from == userId)
-                balance -= blocks[i].transaction.amount;
+        BlockNode* current = head;
+        while (current != nullptr) {
+            if (current->transaction.to == userId)
+                balance += current->transaction.amount;
+            if (current->transaction.from == userId)
+                balance -= current->transaction.amount;
+            current = current->next;
         }
 
         cout << "Balance of user " << userId << " is: " << balance << endl;
@@ -119,9 +211,8 @@ public:
 };
 
 int main() {
-    User users[10];
+    UserList users;
     Blockchain chain;
-    int userCount = 0;
     int option;
 
     while (true) {
@@ -143,26 +234,24 @@ int main() {
 
         switch (option) {
             case 1:
-                if (userCount >= 10) {
-                    cout << "User limit reached!" << endl;
-                    break;
-                }
-                users[userCount] = User(userCount);
-                cout << "User created with ID: " << users[userCount].id << ", Balance: " << users[userCount].balance << endl;
-                userCount++;
+                users.addUser(users.size);
+                cout << "User created with ID: " << users.size - 1 << ", Balance: 0" << endl;
                 break;
 
             case 2:
                 cout << "Enter user ID: ";
                 cin >> id;
-                if (id < 0 || id >= userCount) {
-                    cout << "Invalid user ID!" << endl;
-                    break;
+                {
+                    UserNode* user = users.findUser(id);
+                    if (user == nullptr) {
+                        cout << "Invalid user ID!" << endl;
+                        break;
+                    }
+                    cout << "Enter amount to add: ";
+                    cin >> amount;
+                    user->balance += amount;
+                    cout << "User " << id << " new balance: " << user->balance << endl;
                 }
-                cout << "Enter amount to add: ";
-                cin >> amount;
-                users[id].balance += amount;
-                cout << "User " << id << " new balance: " << users[id].balance << endl;
                 break;
 
             case 3:
@@ -172,24 +261,27 @@ int main() {
                 cin >> to;
                 cout << "Amount: ";
                 cin >> amount;
-                if (from < 0 || from >= userCount || to < 0 || to >= userCount) {
-                    cout << "Invalid user ID(s)!" << endl;
-                    break;
+                {
+                    UserNode* sender = users.findUser(from);
+                    UserNode* receiver = users.findUser(to);
+
+                    if (sender == nullptr || receiver == nullptr) {
+                        cout << "Invalid user ID(s)!" << endl;
+                        break;
+                    }
+                    if (sender->balance < amount) {
+                        cout << "Insufficient balance!" << endl;
+                        break;
+                    }
+                    sender->balance -= amount;
+                    receiver->balance += amount;
+                    chain.addTransaction(from, to, amount);
+                    cout << "Transaction complete.\n";
                 }
-                if (users[from].balance < amount) {
-                    cout << "Insufficient balance!" << endl;
-                    break;
-                }
-                users[from].balance -= amount;
-                users[to].balance += amount;
-                chain.addTransaction(from, to, amount);
-                cout << "Transaction complete.\n";
                 break;
 
             case 4:
-                for (int i = 0; i < userCount; i++) {
-                    users[i].print();
-                }
+                users.printAll();
                 break;
 
             case 5:
@@ -205,14 +297,17 @@ int main() {
             case 7:
                 cout << "Enter user ID: ";
                 cin >> id;
-                if (id < 0 || id >= userCount) {
-                    cout << "Invalid user ID!" << endl;
-                    break;
+                {
+                    UserNode* user = users.findUser(id);
+                    if (user == nullptr) {
+                        cout << "Invalid user ID!" << endl;
+                        break;
+                    }
+                    cout << "Tracked balance from blockchain:\n";
+                    chain.displayBalance(id);
+                    cout << "Actual user balance:\n";
+                    user->print();
                 }
-                cout << "Tracked balance from blockchain:\n";
-                chain.displayBalance(id);
-                cout << "Actual user balance:\n";
-                users[id].print();
                 break;
 
             default:
@@ -224,4 +319,3 @@ int main() {
     cout << "Exiting program.\n";
     return 0;
 }
-
